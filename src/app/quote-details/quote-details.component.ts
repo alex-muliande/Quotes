8f6646eb29534d25af5950edb8a775a17f801de5
alex-muliande <amuliande@gmail.com>
2019-08-18 23:18:12 +0300

    add the delete quote button functionality.Added other file changes.

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+@Input() quote: Quote;
+@Output() isComplete = new EventEmitter<boolean>();
 
+quoteComplete(complete:boolean){
+  this.isComplete.emit(complete);
+}
   constructor() { }
 
   ngOnInit() {
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
-  
+
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  quotes:Quote []= [
-    {id:1, name:'Life Quote',description:'Life is full of life when you join the troops'},
-    {id:2, name: 'Social Quote',description:'Life is full of life when you join the troops'}
+  quotes: Quote[] = [
+    {id:1,name: 'Life Quote',description:'Life is full of life when you join the troops'},
+    {id:2,name: 'Social Quote',description: 'Life is full of life when you join the troops'}
   ];
+
+  completeQuote(isComplete, index){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
+  // toggleDetails(index){
+  //   this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  // }
   constructor() { }
 
   ngOnInit() {
