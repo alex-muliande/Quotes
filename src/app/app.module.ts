63879646f57cdb0af287b2602c618e64cadcc337
alex-muliande <amuliande@gmail.com>
2019-08-19 00:06:23 +0300

    Add Strikethrough files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
