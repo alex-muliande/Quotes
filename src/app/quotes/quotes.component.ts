11d6e0b2d7871097f6feeada97cf0c1dfefa62c5
alex-muliande <amuliande@gmail.com>
2019-08-19 12:37:45 +0300

    Add delete function,This will propmt user if sure to delete the Quote.#

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,7 @@ export class QuotesComponent implements OnInit {
     {id:1,name: 'Life Quote',description:'Keep looking up thats the secret of life-Hancel Asher',upvotes:0,downvotes:0},
     {id:2,name: 'Social Quote',description: 'Those who realize their folly are not true fools -Victor Manuel',upvotes:0,downvotes:0}
   ];
-  quoteDelete(isComplete, index){
+  deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
       if (toDelete){
