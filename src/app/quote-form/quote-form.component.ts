639aaa02a2e4a1ed7193393b9899b45f0b3bb21c
alex-muliande <amuliande@gmail.com>
2019-08-19 10:40:39 +0300

    Modified and made changes in the forms.

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
